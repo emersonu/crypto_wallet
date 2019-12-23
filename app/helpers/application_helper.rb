1eb3b957f9a45803e56d0d3efeb5fda519ae05c3
Emerson <emersonmarques@infarma.com.br>
2019-12-23 17:04:33 -0300

    testando algumasferramentas do rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def data_br(data_us)
+    data_us.strftime("%d/%m/%Y")
+  end
+
+  def ambiente_rails
+    if Rails.env.production?
+      'produção'
+    elsif Rails.env.development?
+      'desenvolvimento'
+    elsif Rails.env.test?
+      'teste'
+    else
+      'outro'
+    end
+  end
+
 end
+
