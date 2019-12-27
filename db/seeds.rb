885ab58ef4f09aa513c8b4dcd3666e29ac091ae0
Emerson <emersonmarques@infarma.com.br>
2019-12-27 09:48:49 -0300

    aprimorando seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Cadastrando as Moedas"
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas ....")
+spinner.auto_spin
 
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "http://www.pngall.com/wp-content/uploads/1/Bitcoin-PNG-Pic.png"
-)
+    coins = [
+                {
+                  description: "Bitcoin",
+                  acronym: "BTC",
+                  url_image: "http://www.pngall.com/wp-content/uploads/1/Bitcoin-PNG-Pic.png"},
 
-Coin.create!(
-    description: "Etherium",
-    acronym: "ETH",
-    url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png"
-)
+                {
+                  description: "Etherium",
+                  acronym: "ETH",
+                  url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png"},
 
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://cdn.freebiesupply.com/logos/large/2x/dash-3-logo-png-transparent.png"
-)
+                {
+                  description: "Dash",
+                  acronym: "DASH",
+                  url_image: "https://cdn.freebiesupply.com/logos/large/2x/dash-3-logo-png-transparent.png"}
+           ]
 
-puts "Moedas Cadastradas com sucesso!"
+coins.each do |coin|
+  Coin.find_or_create_by! coin
+end
+
+spinner.success("Moedas Cadastradas com sucesso!")
